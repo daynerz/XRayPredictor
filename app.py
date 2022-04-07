ab1ef60954efb596624b1eed921bb0478fe39881
Dana Zheng <zhengdana1270@gmail.com>
2022-04-06 23:38:18 -0400

    decrease epoch for local server and add some css

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 import os
 from main import getPrediction
+from keys import staticFolder_path
 
 app = Flask(__name__)
+folder_path = staticFolder_path()
 
 @app.route('/')
 def index():
@@ -10,18 +12,15 @@ def index():
 
 @app.route("/predict", methods = ['GET','POST'])
 def predict():
-    
     if request.method == 'POST':
-        
         file = request.files['file']
         filename = file.filename
-        file_path = os.path.join(r'C:/Users/zheng/OneDrive/Documents/PlayingProjects/CovidPneumonia/static/', filename)                       #slashes should be handeled properly
+        file_path = os.path.join(folder_path, filename) 
         file.save(file_path)
         print(file_path)
         print(filename)
         output = getPrediction(file_path)
         print(output)
-        
     return render_template('predict.html', prediction = output, user_image = '/static/'+filename, file_name=filename)  
 
 if __name__ == "__main__":
